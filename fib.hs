5c6ea1118b08720d3a223409da410d9ce0268b5c
Elliot Skogsberg <elliot.skogsberg@gmail.com>
2024-02-09 11:50:59 +0100

    format changes

--- fib.hs
+++ fib.hs
@@ -2,11 +2,11 @@ import System.Environment(getArgs)
 
 main :: IO ()
 main = do
-    args :: [String] <- getArgs 
+    args :: [String] <- getArgs
     let n = map read args :: [Integer]
     print (map fib n)
 
 fib :: Integer -> Integer
 fib n
-    | n <= 2 = 1
+    | n <= 2    = 1
     | otherwise = fib (n - 1) + fib (n - 2)
